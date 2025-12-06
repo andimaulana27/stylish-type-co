1f9dc7d12600796b731f2d0bdc8e176f3f79a667
andimaulana27 <andymaulana570@gmail.com>
2025-12-06 11:23:57 +0700

    update: meta pixel id script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -75,7 +75,7 @@ export default async function RootLayout({
         {/* --- 5. Tambahkan Meta Pixel Script di sini --- */}
         {config?.meta_pixel_id && (
           <>
-            <Script id="meta-pixel-script" strategy="lazyOnload">
+            <Script id="meta-pixel-script" strategy="beforeInteractive">
               {`
                 !function(f,b,e,v,n,t,s)
                 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
